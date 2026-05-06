8a02a469e1de69a9fff2337496772a8ec470a0d7
Divya Shrivastava <shrivastavadivya785@gmail.com>
2026-05-06 19:13:35 +0530

    Initial Code

---
+++ TEST Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TEST Demo
+//
+//  Created by Divya Shrivastava on 06/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TEST Demo/TEST_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEST_DemoApp.swift
+//  TEST Demo
+//
+//  Created by Divya Shrivastava on 06/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TEST_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
